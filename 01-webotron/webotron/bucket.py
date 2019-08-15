0b70e7bc0e860c3e7dd4a3f4419d44307dd3251c
daniel millikan <danielmillikan@pro.local>
2019-08-15 11:50:43 -0500

    refactor

---
+++ 01-webotron/webotron/bucket.py
@@ -0,0 +1,114 @@
+# -*- code utf-8 -*-
+
+"""Classes for S3 Buckets"""
+
+from botocore.exceptions import ClientError
+import mimetypes
+from pathlib import Path
+
+class BucketManager:
+    """Manage an S3 Bucket"""
+
+    def __init__(self,session):
+        """Creates a BucketManger objcet""" 
+        self.session = session
+        self.s3 = self.session.resource('s3')
+        
+    def all_buckets(self,region=None):
+        """Get an iterator of all buckets"""
+        return self.s3.buckets.all()
+  
+    def all_objects(self,bucket):
+        """Get an iterator of all objects within a bucket"""
+        return self.s3.Bucket(bucket).objects.all()
+
+    def init_bucket(self,bucket_name,region=None):
+        """Initialzie S3 Bucket"""
+
+        try:
+            if region and region.lower() != 'us-east-1':
+                s3_bucket = self.s3.create_bucket(
+                    Bucket=bucket_name,
+                    CreateBucketConfiguration={"LocationConstraint": region}
+                )
+            else:
+                s3_bucket = self.s3.create_bucket(Bucket=bucket_name)
+        
+        except ClientError as e:
+            if e.response['Error']['Code'] == "BucketAlreadyOwnedByYou":
+                s3_bucket = self.s3.bucket(bucket_name)
+            else:
+                raise e
+           
+        return s3_bucket
+
+    def give_public_access(self,bucket):
+        """Give S3 bucket public access"""            
+       
+        pol = bucket.Policy()
+        polstr = """{
+                "Version":"2012-10-17",
+                "Statement":[
+                    {
+                    "Sid":"AddPerm",
+                    "Effect":"Allow",
+                    "Principal": "*",
+                    "Action":["s3:GetObject"],
+                    "Resource":["arn:aws:s3:::%s/*"]
+                    }
+                ]
+            }""" % bucket.name
+        pol.put(Policy=polstr)
+
+        return
+
+
+    def host_website(self,bucket,region=None):
+        """Host Website from Bucket"""
+        if not region:
+            region = 'us-east-1'
+        ws = bucket.Website()
+        ws.put(WebsiteConfiguration={
+            'ErrorDocument': {
+                'Key': 'error.html'
+            },
+            'IndexDocument': {
+                'Suffix': 'index.html'
+            }})
+        url = "http://{0}.s3-website-{1}.amazonaws.com".format(bucket.name, region)
+        print(url)
+        return
+
+    def get_local_path(self,path, root, bucket_name=None):
+        
+        s3_bucket = self.init_bucket(bucket_name)
+
+        for p in path.iterdir():
+            if p.is_dir():
+                self.get_local_path(p, root, bucket_name)
+            if p.is_file():
+                if not bucket_name:
+                    print('File {0} has a key of {1}'.format(
+                        p, p.relative_to(root)))
+                else:
+                    print('Uploading {0} with key {1} to {2}'.format(
+                        p, p.relative_to(root), s3_bucket))
+                    self.upload_file(s3_bucket,
+                                str(p), str(p.relative_to(root)))
+            #if p.is_file() :
+        return
+
+    def upload_file(self, bucket, path, key):
+        content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+        bucket.upload_file(
+            path,
+            key,
+            ExtraArgs={'ContentType': content_type}
+        )
+        return
+
+
+    def sync_path(self, pathname, bucket_name):
+        """Synchronize Local Path to S3 Bucket"""
+        path = Path(pathname).expanduser().resolve()
+        self.get_local_path(path, path, bucket_name)
--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -9,74 +9,18 @@
         - Deploy local files to them
 """
 
-import mimetypes
-from pathlib import Path
-
 import boto3
 import botocore
 import click
 from botocore.exceptions import ClientError
 
+from bucket import BucketManager
+
 session = boto3.Session(profile_name='automation')
-s3 = session.resource('s3')
+bucket_manager = BucketManager(session)
+
 client = boto3.client('s3')
 
-#######################################################################################################
-def bucket_give_public_access(name,region):
-    """Give s3 bucket pubic access"""
-    b = s3.Bucket(name)
-    pol = b.Policy()
-    polstr = """{
-  "Version":"2012-10-17",
-  "Statement":[
-    {
-      "Sid":"AddPerm",
-      "Effect":"Allow",
-      "Principal": "*",
-      "Action":["s3:GetObject"],
-      "Resource":["arn:aws:s3:::%s/*"]
-    }
-  ]
-}""" % name
-    pol.put(Policy=polstr)
-    bucket_make_website(name,region)
-    return
-#######################################################################################################
-def bucket_make_website(name,region):
-    """Give s3 bucket pubic access"""
-    b = s3.Bucket(name)
-    ws = b.Website()
-    ws.put(WebsiteConfiguration={
-        'ErrorDocument': {
-            'Key': 'error.html'
-        },
-        'IndexDocument': {
-            'Suffix': 'index.html'
-        }})
-    url = "http://{0}.s3-website-{1}.amazonaws.com".format(name , region)
-    print(url)
-    return
-#######################################################################################################
-def get_local_path(path,root,bucket_name=None):
-    for p in path.iterdir():
-        if p.is_dir() : get_local_path(p,root,bucket_name)
-        if p.is_file() : 
-            if not bucket_name: 
-                print('File {0} has a key of {1}'.format(p,p.relative_to(root)))
-            else : 
-                print('Uploading {0} with key {1} to {2}'.format(p,p.relative_to(root),s3.Bucket(bucket_name)))
-                upload_file(s3.Bucket(bucket_name),str(p),str(p.relative_to(root)))
-        #if p.is_file() :
-    return
-#######################################################################################################
-def upload_file(s3_bucket,path,key):
-    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
-    s3_bucket.upload_file(
-        path,
-        key,
-        ExtraArgs={'ContentType':content_type}
-    )
-    return   
 #######################################################################################################
 #######################################################################################################
 #######################################################################################################
@@ -90,12 +34,20 @@ def buckets():
 #######################################################################################################
 @buckets.command("sync")
 @click.argument("pathname",type=click.Path(exists=True))
-@click.option("--bucket", "bucket", default=None)
-def sync_path(pathname,bucket):
+@click.option("--bucket", "bucket_name", default=None)
+def sync_path(pathname,bucket_name):
     """Synchronize Local Path to S3 Bucket"""
-    path = Path(pathname).expanduser().resolve()
-    get_local_path(path,path,bucket)
-    
+    bucket_manager.sync_path(pathname, bucket_name)
+
+    return
+#######################################################################################################
+@buckets.command("list")
+def list_buckets():
+    """List all s3 buckets"""
+
+    for b in bucket_manager.all_buckets():
+        print(b.name)
+  
     return
 
 #######################################################################################################
@@ -103,29 +55,18 @@ def sync_path(pathname,bucket):
 @click.argument("name")
 @click.option("--region", "region", default='us-east-1', help ='Bucket Region')
 @click.option("--public", "public", default=False, is_flag=True, help ='Make Bucket Public')
-def create_bucket(name,public,region):
+@click.option("--website", "website", default=False, is_flag=True, help='Host Static Website from Bucket')
+def create_bucket(name,public,region,website):
     """Create new s3 bucket"""
 
-    try:
-        if region.lower() != 'us-east-1':
-            s3.create_bucket(
-                Bucket = name,
-                CreateBucketConfiguration={"LocationConstraint":region}
-            )
-        else:
-            s3.create_bucket(Bucket = name)
+    s3_bucket = bucket_manager.init_bucket(name,region)
 
-        print('\n \tBucket {0} created successfully in region {1}\n'.format(name,region))
-
-    except ClientError as e:
-        if e.response['Error']['Code'] == "BucketAlreadyOwnedByYou":
-            print('\n \tBucket {0} already created in region {1}\n'.format(name,region))
-            pass
-        else:
-            raise e
+    if website:
+        public = True
+        bucket_manager.host_website(s3_bucket,region)
 
     if public:
-        bucket_give_public_access(name,region)
+        bucket_manager.give_public_access(s3_bucket)
 
     return
 #######################################################################################################
@@ -137,7 +78,7 @@ def objects():
 @click.argument('bucket')
 def list_bucket_objects(bucket):
     """List objects within s3 bucket"""
-    for okey in s3.Bucket(bucket).objects.all():
+    for okey in bucket_manager.all_objects(bucket):
         print(okey.key)
 
     return
