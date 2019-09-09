e5632e67478a3224c451bf1730c119712c1bfb37
daniel millikan <danielmillikan@pro.local>
2019-09-09 15:01:56 -0500

    post to slack on scaleup

--- 02-notifon/notifier/handler.py
+++ 02-notifon/notifier/handler.py
@@ -1,9 +1,12 @@
+import requests
 import os
 
 
 def post_to_slack(event, context):
     slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
-    print(slack_webhook_url)
+    
+    slack_message = "At {time} an event of type {source} occured.\n{detail[Description]}".format(**event)
+    data = {"text": slack_message}
+    requests.post(slack_webhook_url, json=data)
 
     return
-    
